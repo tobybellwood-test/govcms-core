f9095cc31ed28fc5402fc00c4ced46248450ae55
Joseph Zhao <joseph.zhao@xing.net.au>
2017-03-24 16:46:53 +1100

    Add Custom Form placeholder for installation

---
+++ src/Form/govCMSInstallerForm.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * govCMS Custom Installer Form.
+ */
+
+namespace Drupal\govcms\Installer\Form;
+
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Extension\ModuleInstallerInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class govCMSInstallerForm
+ *
+ * @package Drupal\govcms\Installer\Form
+ */
+class govCMSInstallerForm extends FormBase {
+
+}
