31ece22d9d4bf682eccd742fb5099108b3639230
Joseph Zhao <joseph.zhao@xing.net.au>
2017-04-10 14:36:50 +1000

    Optional modules are added in govcms.info.yml file under dependencies_optional section

--- src/Installer/Form/govCMSInstallerForm.php
+++ src/Installer/Form/govCMSInstallerForm.php
@@ -12,6 +12,7 @@ use Drupal\Core\Extension\ModuleInstallerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class govCMSInstallerForm
@@ -55,28 +56,33 @@ class govCMSInstallerForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['#title'] = t('Optional');
+    $form['#title'] = t('Optional modules');
+    $install_state = $form_state->getBuildInfo()['args'][0];
 
-    $form['govcms_blog'] = [
-      '#type' => 'checkbox',
-      '#title' => 'govCMS blog article',
-      '#description' => t("Defines blog article content type"),
-      '#default_value' => TRUE,
-    ];
-
-    $form['govcms_event'] = [
-      '#type' => 'checkbox',
-      '#title' => 'govCMS event',
-      '#description' => t("Defines event content type"),
-      '#default_value' => TRUE,
-    ];
-
-    $form['govcms_news_and_media'] = [
-      '#type' => 'checkbox',
-      '#title' => 'govCMS news and media',
-      '#description' => t("Defines news and media content type"),
-      '#default_value' => TRUE,
-    ];
+    // If we have any configurable_dependencies in the profile then show them
+    // to the user so they can be selected.
+    if (!empty($install_state['profile_info']['dependencies_optional'])) {
+      $form['modules_optional'] = [
+        '#type' => 'container',
+        '#tree' => TRUE,
+      ];
+      foreach ($install_state['profile_info']['dependencies_optional'] as $module_name) {
+        $module_path = 'profiles/contrib/govcms/modules/custom/govcms_optional_modules/' . $module_name . '/' . $module_name . '.info.yml';
+        if ($module_info_file = file_get_contents($module_path)) {
+          $module_info = Yaml::parse($module_info_file);
+          $form['modules_optional'][$module_name] = [
+            '#title' => $module_info['name'],
+            '#description' => !empty($module_info['description']) ? $module_info['description'] : '',
+            '#type' => 'checkbox',
+            '#default_value' => !empty($module_info['enabled']),
+          ];
+        }
+        continue;
+      }
+    }
+    else {
+      $form['#suffix'] = $this->t('There are no available modules at this time.');
+    }
 
     $form['warning'] = [
       '#markup' => "<p><strong>Warning:</strong> Don't install the optional modules if you're upgrading from Drupal 7 - you need to start from a blank site.</p>",
@@ -97,29 +103,13 @@ class govCMSInstallerForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $input = $form_state->getUserInput();
-    if (isset($input['govcms_blog'])) {
-      $govcms_blog = !empty($input['govcms_blog']);
-    }
-
-    if (isset($input['govcms_event'])) {
-      $govcms_event = !empty($input['govcms_event']);
-    }
-
-      if (isset($input['govcms_news_and_media'])) {
-          $govcms_news_and_media = !empty($input['govcms_news_and_media']);
-      }
-
-    if ($govcms_blog) {
-      $this->moduleInstaller->install(['govcms_content_types_govcms_blog_article']);
-    }
-
-    if ($govcms_event) {
-      $this->moduleInstaller->install(['govcms_content_types_govcms_event']);
-    }
-
-    if ($govcms_news_and_media) {
-      $this->moduleInstaller->install(['govcms_content_types_govcms_news_and_media']);
+    $modules = array_filter($form_state->getValue('modules_optional'), function ($enabled) {
+      return (bool) $enabled;
+    });
+    // Install optional modules.
+    if (!empty($modules) && is_array($modules)) {
+      $this->moduleInstaller->install(array_keys($modules));
     }
   }
+
 }
